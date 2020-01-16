b2789d42c601e06aa94c9346bf059ee81b34232c
Linxius <lqlin96@163.com>
2020-01-16 18:35:30 +0800

    code cleared

--- HPCnet/src/get_hausdorff_dis_gpu.cu
+++ HPCnet/src/get_hausdorff_dis_gpu.cu
@@ -79,64 +79,6 @@ __global__ void get_hausdorff_dis_kernel_fast(const float *__restrict__ whole_po
         hsdf_dis = prior_to_dis > to_prior_dis? prior_to_dis : to_prior_dis;
         features[gt_idx]  = hsdf_dis > radius? 0 : (radius-hsdf_dis) / radius;
     }
-    // dim3 blocks(DIVUP(point_num, THREADS_PER_BLOCK), batch_size);  // blockIdx.x(col), blockIdx.y(row)
-    // dim3 threads(gt_num, DIVUP(THREADS_PER_BLOCK, gt_num));
-
-    /*
-    int batch_idx = blockIdx.y;
-    int point_idx = blockIdx.x * blockDim.y + threadIdx.y;
-    int gt_idx = threadIdx.x;
-
-    // keypoints = batch_idx * keypoint_num * 3 + point_idx * 3;
-    // whole_points += batch_idx * whole_point_num * 3;
-    neighbor_points += batch_idx * keypoint_num * neighbor_point_num * 3 + point_idx * neighbor_point_num * 3;
-    features += batch_idx * keypoint_num * gt_num + point_idx * gt_num + gt_idx;
-    dis_dicts += gt_idx * dict_grid_num;
-    prior_points += gt_idx * prior_point_num * 3;
-
-    // float r2 = radius * radius;
-    // float keypoint_x = keypoints[0];
-    // float keypoint_y = keypoints[1];
-    // float keypoint_z = keypoints[2];
-
-    float to_prior_dis = 0;
-    float tmp_dis;
-    int xth, yth, zth;
-    int i;
-    int prior_hash_idx;
-    for( i = 0; i < neighbor_point_num; i++ ){
-        xth = floor(abs(neighbor_points[i*3 + 0] + radius) / voxel_len);
-        yth = floor(abs(neighbor_points[i*3 + 1] + radius) / voxel_len);
-        zth = floor(abs(neighbor_points[i*3 + 2] + radius) / voxel_len);
-        prior_hash_idx = xth + yth * voxel_dim + zth * voxel_dim * voxel_dim;
-        tmp_dis = dis_dicts[prior_hash_idx];
-        if( to_prior_dis < tmp_dis ){
-            to_prior_dis = tmp_dis;
-        }
-    }
-
-    float prior_to_dis = 0;
-    float min_point_pair_dis;
-    int j;
-    for( i = 0; i < prior_point_num; i++ ){
-        min_point_pair_dis = 99.9;
-        for( j = 0; j < neighbor_point_num; j++ ){
-            tmp_dis = ( pow(prior_points[i*3 + 0] - neighbor_points[j*3 + 0], 2) +
-                        pow(prior_points[i*3 + 1] - neighbor_points[j*3 + 1], 2) +
-                        pow(prior_points[i*3 + 2] - neighbor_points[j*3 + 2], 2) );
-            if( min_point_pair_dis > tmp_dis ){
-                min_point_pair_dis = tmp_dis;
-            }
-        }
-        if( min_point_pair_dis > prior_to_dis ){
-            prior_to_dis = min_point_pair_dis;
-        }
-    }
-    prior_to_dis = sqrt(prior_to_dis);
-
-    float hsdf_dis = prior_to_dis > to_prior_dis? prior_to_dis : to_prior_dis;
-    *features  = hsdf_dis > radius? 0 : (radius-hsdf_dis) / radius;
-    */
 }
 
 void get_hausdorff_dis_kernel_launcher_fast(const float* whole_points, const float* keypoints,
@@ -158,10 +100,6 @@ void get_hausdorff_dis_kernel_launcher_fast(const float* whole_points, const flo
 
     dim3 blocks(DIVUP(keypoint_num, THREADS_PER_BLOCK), batch_size);  // blockIdx.x(col), blockIdx.y(row)
     dim3 threads(THREADS_PER_BLOCK);
-    // ball_query_kernel_fast<<<blocks, threads, 0, stream>>>(b, n, m, radius, nsample, new_xyz, xyz, idx);
-
-    // dim3 blocks(DIVUP(keypoint_num, THREADS_PER_BLOCK), batch_size);
-    // dim3 threads(gt_num, DIVUP(THREADS_PER_BLOCK, gt_num));
 
     get_hausdorff_dis_kernel_fast<<<blocks, threads, 0, stream>>>(
         whole_points, keypoints, neighbor_points, features, radius, batch_size, whole_point_num,
